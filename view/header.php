12ac40a8faf3c07e312480360755218cee8de956
Vassili Simon <vassili.simon@gmail.com>
2017-07-20 14:09:16 +0200

    Latest update

---
+++ view/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>MVDB &#124; Yet Another Movie Collection Manager</title>
+        <script type="text/javascript" src="../public/lib/jquery/jquery-3.2.1.min.js"></script>
+        <script type="text/javascript" src="../public/lib/materialize/js/materialize.min.js"></script>
+        <link type="text/css" href="../public/lib/materialize/css/materialize.min.css" />
+    </head>
+    <body>
+        <nav>
+            <div class="nav-wrapper">
+                <a href="#" class="right">
+                    <input type="text" placeholder="search" />
+                </a>
+                <ul id="nav-mobile" class="left hide-on-med-and-down">
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Genre</a></li>
+                    <li><a href="">Add a Movie</a></li>
+                </ul>
+            </div>
+        </nav>
