83e473ee1bb03b1c9ae2b8f1f644940bad711308
John Shantwo <mrshantwo@gmail.com>
2017-07-21 16:35:19 +0200

    read WIP

--- view/list.php
+++ view/list.php
@@ -1,3 +1,4 @@
+<script lang="javascript" type="text/javascript" src="js/list_modal_init.js"></script>
 <div class="container">
     <?php foreach($Array_list as $value):?>
         <div class="card horizontal">
@@ -12,7 +13,7 @@
                     <p><?= $value['mov_synopsis'] ?></p>
                 </div>
                 <div class="card-action">
-                    <a href="#modal1?id=<?= $value['mov_id'] ?>">SEE DETAILS</a>
+                    <a href="#modal1" class="btn modal-trigger" name="<?= $value['mov_id']?>">SEE DETAILS</a>
                 </div>
             </div>
         </div>
@@ -20,14 +21,35 @@
 </div>
 
 <!-- Modal Structure -->
-<div id="modal1" class="modal">
+<div id="modal1" class="modal modal-fixed-footer">
     <div class="modal-content">
-        <h4>Modal Header</h4>
-        <p>A bunch of text</p>
+
     </div>
     <div class="modal-footer">
-        <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+        <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat ">Agree</a>
     </div>
 </div>
 
-<script lang="javascript" type="text/javascript" src="js/list_modal_init.js">
+<script type="text/javascript">
+    $(".btn").on("click", function(){
+        var id = this.name;
+        modalcontent(id);
+    });
+
+    function modalcontent(valeurId){
+        $(".modal-content").html("");
+            $.ajax({
+                type:'POST',
+                url: "ajax/read.php",
+                dataType : 'json',
+                data: {'id':id}
+            })
+            .done(function(data) {
+                print_r(data);
+            })
+            .fail(function() {
+                alert("Bad news BRO, some gremlins ate your code!!");
+            });
+        }
+    }
+</script>
